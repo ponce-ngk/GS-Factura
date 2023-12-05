8d6aea263e83032aefdcdb8cd88225447beda86e
kevin <kevin@KEVIN>
2023-12-05 12:07:41 -0500

    Agregar archivos de proyecto.

---
+++ GS-Factura/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace GS_Factura
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelLayoutMenu = new System.Windows.Forms.TableLayoutPanel();
+            this.panelDashCenter = new System.Windows.Forms.Panel();
+            this.panelDashHead = new System.Windows.Forms.Panel();
+            this.panelDashOut = new System.Windows.Forms.Panel();
+            this.panelLayoutForms = new System.Windows.Forms.TableLayoutPanel();
+            this.panelForms = new System.Windows.Forms.Panel();
+            this.panelLayoutFormsHead = new System.Windows.Forms.TableLayoutPanel();
+            this.panelButtomDash = new System.Windows.Forms.Panel();
+            this.panelDashHeadCenter = new System.Windows.Forms.Panel();
+            this.panelDashExit = new System.Windows.Forms.Panel();
+            this.lblEncabezadoGS = new System.Windows.Forms.Label();
+            this.panelLayoutMenu.SuspendLayout();
+            this.panelLayoutForms.SuspendLayout();
+            this.panelLayoutFormsHead.SuspendLayout();
+            this.panelDashHeadCenter.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelLayoutMenu
+            // 
+            this.panelLayoutMenu.ColumnCount = 1;
+            this.panelLayoutMenu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.panelLayoutMenu.Controls.Add(this.panelDashCenter, 0, 1);
+            this.panelLayoutMenu.Controls.Add(this.panelDashHead, 0, 0);
+            this.panelLayoutMenu.Controls.Add(this.panelDashOut, 0, 2);
+            this.panelLayoutMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLayoutMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelLayoutMenu.Name = "panelLayoutMenu";
+            this.panelLayoutMenu.RowCount = 3;
+            this.panelLayoutMenu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.panelLayoutMenu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.panelLayoutMenu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.panelLayoutMenu.Size = new System.Drawing.Size(113, 473);
+            this.panelLayoutMenu.TabIndex = 0;
+            // 
+            // panelDashCenter
+            // 
+            this.panelDashCenter.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panelDashCenter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDashCenter.Location = new System.Drawing.Point(0, 70);
+            this.panelDashCenter.Margin = new System.Windows.Forms.Padding(0);
+            this.panelDashCenter.Name = "panelDashCenter";
+            this.panelDashCenter.Size = new System.Drawing.Size(113, 378);
+            this.panelDashCenter.TabIndex = 0;
+            // 
+            // panelDashHead
+            // 
+            this.panelDashHead.BackColor = System.Drawing.Color.Silver;
+            this.panelDashHead.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDashHead.Location = new System.Drawing.Point(0, 0);
+            this.panelDashHead.Margin = new System.Windows.Forms.Padding(0);
+            this.panelDashHead.Name = "panelDashHead";
+            this.panelDashHead.Size = new System.Drawing.Size(113, 70);
+            this.panelDashHead.TabIndex = 1;
+            // 
+            // panelDashOut
+            // 
+            this.panelDashOut.BackColor = System.Drawing.Color.RosyBrown;
+            this.panelDashOut.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDashOut.Location = new System.Drawing.Point(0, 448);
+            this.panelDashOut.Margin = new System.Windows.Forms.Padding(0);
+            this.panelDashOut.Name = "panelDashOut";
+            this.panelDashOut.Size = new System.Drawing.Size(113, 25);
+            this.panelDashOut.TabIndex = 2;
+            // 
+            // panelLayoutForms
+            // 
+            this.panelLayoutForms.ColumnCount = 1;
+            this.panelLayoutForms.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.panelLayoutForms.Controls.Add(this.panelForms, 0, 1);
+            this.panelLayoutForms.Controls.Add(this.panelLayoutFormsHead, 0, 0);
+            this.panelLayoutForms.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelLayoutForms.Location = new System.Drawing.Point(113, 0);
+            this.panelLayoutForms.Name = "panelLayoutForms";
+            this.panelLayoutForms.RowCount = 2;
+            this.panelLayoutForms.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.panelLayoutForms.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 95F));
+            this.panelLayoutForms.Size = new System.Drawing.Size(722, 473);
+            this.panelLayoutForms.TabIndex = 1;
+            // 
+            // panelForms
+            // 
+            this.panelForms.BackColor = System.Drawing.Color.LavenderBlush;
+            this.panelForms.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelForms.Location = new System.Drawing.Point(0, 23);
+            this.panelForms.Margin = new System.Windows.Forms.Padding(0);
+            this.panelForms.Name = "panelForms";
+            this.panelForms.Size = new System.Drawing.Size(722, 450);
+            this.panelForms.TabIndex = 1;
+            // 
+            // panelLayoutFormsHead
+            // 
+            this.panelLayoutFormsHead.ColumnCount = 3;
+            this.panelLayoutFormsHead.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.panelLayoutFormsHead.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85F));
+            this.panelLayoutFormsHead.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.panelLayoutFormsHead.Controls.Add(this.panelDashExit, 2, 0);
+            this.panelLayoutFormsHead.Controls.Add(this.panelDashHeadCenter, 1, 0);
+            this.panelLayoutFormsHead.Controls.Add(this.panelButtomDash, 0, 0);
+            this.panelLayoutFormsHead.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelLayoutFormsHead.Location = new System.Drawing.Point(0, 0);
+            this.panelLayoutFormsHead.Margin = new System.Windows.Forms.Padding(0);
+            this.panelLayoutFormsHead.Name = "panelLayoutFormsHead";
+            this.panelLayoutFormsHead.RowCount = 1;
+            this.panelLayoutFormsHead.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.panelLayoutFormsHead.Size = new System.Drawing.Size(722, 23);
+            this.panelLayoutFormsHead.TabIndex = 2;
+            // 
+            // panelButtomDash
+            // 
+            this.panelButtomDash.BackColor = System.Drawing.Color.LightCoral;
+            this.panelButtomDash.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelButtomDash.Location = new System.Drawing.Point(0, 0);
+            this.panelButtomDash.Margin = new System.Windows.Forms.Padding(0);
+            this.panelButtomDash.Name = "panelButtomDash";
+            this.panelButtomDash.Size = new System.Drawing.Size(36, 23);
+            this.panelButtomDash.TabIndex = 0;
+            // 
+            // panelDashHeadCenter
+            // 
+            this.panelDashHeadCenter.BackColor = System.Drawing.Color.White;
+            this.panelDashHeadCenter.Controls.Add(this.lblEncabezadoGS);
+            this.panelDashHeadCenter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDashHeadCenter.Location = new System.Drawing.Point(36, 0);
+            this.panelDashHeadCenter.Margin = new System.Windows.Forms.Padding(0);
+            this.panelDashHeadCenter.Name = "panelDashHeadCenter";
+            this.panelDashHeadCenter.Size = new System.Drawing.Size(613, 23);
+            this.panelDashHeadCenter.TabIndex = 1;
+            // 
+            // panelDashExit
+            // 
+            this.panelDashExit.BackColor = System.Drawing.Color.LightBlue;
+            this.panelDashExit.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDashExit.Location = new System.Drawing.Point(649, 0);
+            this.panelDashExit.Margin = new System.Windows.Forms.Padding(0);
+            this.panelDashExit.Name = "panelDashExit";
+            this.panelDashExit.Size = new System.Drawing.Size(73, 23);
+            this.panelDashExit.TabIndex = 2;
+            // 
+            // lblEncabezadoGS
+            // 
+            this.lblEncabezadoGS.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblEncabezadoGS.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEncabezadoGS.Location = new System.Drawing.Point(32, 0);
+            this.lblEncabezadoGS.Name = "lblEncabezadoGS";
+            this.lblEncabezadoGS.Size = new System.Drawing.Size(552, 23);
+            this.lblEncabezadoGS.TabIndex = 0;
+            this.lblEncabezadoGS.Text = "Gestión del Software - Sistema de Facturación - v1.0";
+            this.lblEncabezadoGS.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(835, 473);
+            this.Controls.Add(this.panelLayoutForms);
+            this.Controls.Add(this.panelLayoutMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SISTEMA DE FACTURACIÓN";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panelLayoutMenu.ResumeLayout(false);
+            this.panelLayoutForms.ResumeLayout(false);
+            this.panelLayoutFormsHead.ResumeLayout(false);
+            this.panelDashHeadCenter.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel panelLayoutMenu;
+        private System.Windows.Forms.Panel panelDashCenter;
+        private System.Windows.Forms.Panel panelDashHead;
+        private System.Windows.Forms.Panel panelDashOut;
+        private System.Windows.Forms.TableLayoutPanel panelLayoutForms;
+        private System.Windows.Forms.Panel panelForms;
+        private System.Windows.Forms.TableLayoutPanel panelLayoutFormsHead;
+        private System.Windows.Forms.Panel panelDashExit;
+        private System.Windows.Forms.Panel panelDashHeadCenter;
+        private System.Windows.Forms.Panel panelButtomDash;
+        private System.Windows.Forms.Label lblEncabezadoGS;
+    }
+}
+
