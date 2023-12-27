05997acb3bc627a212015dbda9576111b2b59f12
ElvisGonzales <elvis.gonzales2016@uteq.edu.ec>
2023-12-26 20:06:46 -0500

    Codigo comentado del producto

--- GS-Factura/GS-RegistroProducto.cs
+++ GS-Factura/GS-RegistroProducto.cs
@@ -18,6 +18,8 @@ namespace GS_Factura
             InitializeComponent();
             LlenarLimpiar();
         }
+
+        //Funcion de Borrar datos en el textbox y el llenado del data Grid con datos actualizar
         public void LlenarLimpiar() {
             txtcantidadproducto.Text = "";
             txtnombreproducto.Text = "";
@@ -29,6 +31,7 @@ namespace GS_Factura
 
         private void dgvProductos_Click(object sender, EventArgs e)
         {
+            //Asignacion de datos de la BD al DataGrid
             try
             {                
                 txtIdProducto.Text = dgvProductos.CurrentRow.Cells[0].Value.ToString();
@@ -43,6 +46,7 @@ namespace GS_Factura
         }
         private void txtbuscarproducto__TextChanged(object sender, EventArgs e)
         {
+            //Busqueda de Producto por el nombre al momento de digitalizar el nombre del producto
             dgvProductos.DataSource = AccesoDatos.llenartablaparabuscar
                 ("select RTRIM(IDPRODUCTO) AS IDPRODUCTO, RTRIM(PRODUCTO) AS PRODUCTO, RTRIM(PRECIO_UNITARIO) AS PRECIO_UNITARIO," +
                 "RTRIM(STOCK) AS STOCK from PRODUCTO WHERE ESTADO = 1 and PRODUCTO like '" + txtbuscarproducto.Texts+"%'");
@@ -50,7 +54,7 @@ namespace GS_Factura
 
         private void txtbuscarproducto_KeyPress(object sender, KeyPressEventArgs e)
         {
-            
+            //Validacion de que sea solo letras y espacio 
                 if (!(char.IsLetter(e.KeyChar) || e.KeyChar == ' ') && (e.KeyChar != (char)Keys.Back))
                 {
                     e.Handled = true;
@@ -59,6 +63,7 @@ namespace GS_Factura
 
         private void txtnombreproducto_KeyPress(object sender, KeyPressEventArgs e)
         {
+            //Validacion de que sea solo letras y espacio
             if (!(char.IsLetter(e.KeyChar) || e.KeyChar == ' ') && (e.KeyChar != (char)Keys.Back))
             {
                 e.Handled = true;
@@ -67,6 +72,7 @@ namespace GS_Factura
 
         private void txtcantidadproducto_KeyPress(object sender, KeyPressEventArgs e)
         {
+            //Validacion de que sea solo numeros  y el punto
             if (!(char.IsNumber(e.KeyChar) || e.KeyChar == '.') && (e.KeyChar != (char)Keys.Back))
             {
                 e.Handled = true;
@@ -75,6 +81,7 @@ namespace GS_Factura
 
         private void txtpreciounitario_KeyPress(object sender, KeyPressEventArgs e)
         {
+            //Validacion de que sea solo numeros  y el punto
             if (!(char.IsNumber(e.KeyChar) || e.KeyChar == '.') && (e.KeyChar != (char)Keys.Back))
             {
                 e.Handled = true;
@@ -83,9 +90,13 @@ namespace GS_Factura
 
         private void btnAgregarProducto_Click(object sender, EventArgs e)
         {
+            //Ingreso de un nuevo producto
+            //Verifica si los txt no estan vacios
             if (txtcantidadproducto.Text != "" && txtnombreproducto.Text != "" && txtpreciounitario.Text != "")
             {
+                //Pregunta si esta de acuerdo en agregar un nuevo producto
                 DialogResult confirmacion = MessageBox.Show("¿Estás seguro de que quieres agregar estos datos?", "Confirmar adición", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                //si presiona si procede con la insersion 
                 if (confirmacion == DialogResult.Yes)
                 {
                     Producto pProducto = new Producto();
@@ -105,6 +116,7 @@ namespace GS_Factura
                     LlenarLimpiar();
                 }
             }
+            //caso contrario advierte que estan vacios los txt
             else
             {
                 MessageBox.Show("campos vacios", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Information);
@@ -113,11 +125,15 @@ namespace GS_Factura
 
         private void btnEliminarProducto_Click(object sender, EventArgs e)
         {
+            //Eliminar un producto
+            //Verifica si los txt no estan vacios
             if (txtIdProducto.Text != "")
             {
+                //Pregunta si esta de acuerdo en eliminar un producto
                 DialogResult confirmacion = MessageBox.Show("¿Estás seguro de que quieres eliminar estos datos?", "Confirmar eliminacion", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
                 if (confirmacion == DialogResult.Yes)
                 {
+                    //si presiona si procede con la eliminacion 
                     Producto pProducto = new Producto();
                     pProducto.IDPRODUCTO1 = int.Parse(txtIdProducto.Text);
                     pProducto.ESTADO1 = '0';
@@ -134,6 +150,7 @@ namespace GS_Factura
                     LlenarLimpiar();
                 }
             }
+            //caso contrario advierte que estan vacios los txt
             else
             {
                 MessageBox.Show("campos vacios", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Information);
@@ -142,11 +159,15 @@ namespace GS_Factura
 
         private void btnActualizarProducto_Click(object sender, EventArgs e)
         {
+            //Modificacion de un producto
+            //Verifica si los txt no estan vacios
             if (txtnombreproducto.Text != "" && txtpreciounitario.Text != "" && txtcantidadproducto.Text != "")
             {
+                //Pregunta si esta de acuerdo en modificar un producto
                 DialogResult confirmacion = MessageBox.Show("¿Estás seguro de que quieres actualizar estos datos?", "Confirmar modificacion", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
                 if (confirmacion == DialogResult.Yes)
                 {
+                    //si presiona si procede con la Modificacion
                     Producto pProducto = new Producto();
                     pProducto.IDPRODUCTO1 = int.Parse(txtIdProducto.Text);
                     pProducto.PRODUCTO1 = txtnombreproducto.Text;
@@ -166,12 +187,13 @@ namespace GS_Factura
                     LlenarLimpiar();
                 }
             }
+            //caso contrario advierte que estan vacios los txt
             else
             {
                 MessageBox.Show("campos vacios", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
-
+        //Funcion para Limpiar todos los campos en caso de no querrer lo escrito.
         private void btnLimpiarProducto_Click(object sender, EventArgs e)
         {
             LlenarLimpiar();
