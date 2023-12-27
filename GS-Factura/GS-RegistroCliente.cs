0c79ff5ace42249f9cbc88896daf68c9f4bd1c66
eduardo96452 <107895582+eduardo96452@users.noreply.github.com>
2023-12-26 19:52:27 -0500

    comentarios

--- GS-Factura/GS-RegistroCliente.cs
+++ GS-Factura/GS-RegistroCliente.cs
@@ -20,22 +20,25 @@ namespace GS_Factura
         {
             InitializeComponent();
             AccesoDatos fr = new AccesoDatos();
+            // Se llena el DataGridView con los datos de los clientes al cargar el formulario
             dgvClientes.DataSource = AccesoDatos.llenartablaparabuscar("sp_Listado_Clientes");
+            // Se establece el formato personalizado de la fecha en el control DateTimePicker para poderlo guardar
             dtpFechaCliente.CustomFormat = "yyyy-MM-dd";
         }
 
         private void btnGuardarDueño_Click(object sender, EventArgs e)
         {
+            // Se valida si hay campos vacíos antes de continuar
             if (string.IsNullOrWhiteSpace(txtcedulacliente.Text) ||
                 string.IsNullOrWhiteSpace(txtnombrescliente.Text) ||
                 string.IsNullOrWhiteSpace(txtapellidoscliente.Text) ||
                 string.IsNullOrWhiteSpace(dtpFechaCliente.Text))
             {
                 MessageBox.Show("Por favor, completa todos los campos antes de continuar.", "Campos vacíos", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-
             }
             else
             {
+                // Se confirmar antes de agregar al cliente
                 DialogResult resultado = MessageBox.Show("¿Estás seguro de que quieres agregar estos datos?", "Confirmar adición", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
 
                 if (resultado == DialogResult.Yes)
@@ -47,7 +50,7 @@ namespace GS_Factura
                     pClientes.Apellido = txtapellidoscliente.Text;
                     pClientes.FechaNA = dtpFechaCliente.Text;
 
-
+                    // Se llama al metodo AgregarCliente de la clase CrudCliente y se envian los datos
                     int resultado2 = CrudCliente.AgregarCliente(pClientes);
 
                     if (resultado2 > 0)
@@ -58,7 +61,8 @@ namespace GS_Factura
                     {
                         MessageBox.Show("No se pudieron Guardar", "Error al guardar", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                     }
-
+                    
+                    // Se actualiza el DataGridView y se limpian los campos
                     dgvClientes.DataSource = AccesoDatos.llenartablaparabuscar("sp_Listado_Clientes");
                     LimpiarCampos();
                 }
@@ -79,7 +83,7 @@ namespace GS_Factura
             {
                 DialogResult resultado = MessageBox.Show("¿Estás seguro de que quieres actualizar estos datos?", "Confirmar actualización", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
 
-                // Verificar la respuesta del usuario
+                // Se confirmar antes de editar al cliente
                 if (resultado == DialogResult.Yes)
                 {
                     Cliente pClientes = new Cliente();
@@ -90,6 +94,7 @@ namespace GS_Factura
                     pClientes.Apellido = txtapellidoscliente.Text;
                     pClientes.FechaNA = dtpFechaCliente.Text;
 
+                    // Se llama al metodo ActualizarClient de la clase CrudCliente y se envian los datos
                     int resultado2 = CrudCliente.ActualizarClient(pClientes);
 
                     if (resultado2 > 0)
@@ -101,6 +106,7 @@ namespace GS_Factura
                         MessageBox.Show("No se pudieron Editar", "Error al editar", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                     }
 
+                    // Actualizar el DataGridView y limpiar los campos
                     dgvClientes.DataSource = AccesoDatos.llenartablaparabuscar("sp_Listado_Clientes");
                     LimpiarCampos();
                 }
@@ -118,6 +124,7 @@ namespace GS_Factura
             }
             else
             {
+                // Se confirmar antes de eliminar al cliente
                 DialogResult resultado = MessageBox.Show("¿Estás seguro de que quieres eliminar los datos de los campos de texto?", "Confirmar eliminación", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
 
                 if (resultado == DialogResult.Yes)
@@ -127,6 +134,7 @@ namespace GS_Factura
                     pClientes.IdCliente = int.Parse(txtidcliente.Text);
                     pClientes.Estado = '0';
 
+                    // Se llama al metodo EliminarClient de la clase CrudCliente y se envian los datos
                     int resultado2 = CrudCliente.EliminarClient(pClientes);
 
                     if (resultado2 > 0)
@@ -138,8 +146,8 @@ namespace GS_Factura
                         MessageBox.Show("No se pudieron Eliminar", "Error al Eliminar", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                     }
 
+                    // Actualizar el DataGridView y limpiar los campos
                     dgvClientes.DataSource = AccesoDatos.llenartablaparabuscar("sp_Listado_Clientes");
-
                     LimpiarCampos();
                 }
                 // Si el usuario elige 'No', no se hace nada
@@ -148,6 +156,7 @@ namespace GS_Factura
 
         private void LimpiarCampos()
         {
+            // Se limpian todos los campos para volverlo ingresar
             txtidcliente.Text = "";
             txtcedulacliente.Text = "";
             txtnombrescliente.Text = "";
@@ -159,6 +168,7 @@ namespace GS_Factura
         {
             try
             {
+                // Mostrar los datos de la fila seleccionada en los campos correspondientes
                 txtidcliente.Text = dgvClientes.CurrentRow.Cells[0].Value.ToString();
                 txtcedulacliente.Text = dgvClientes.CurrentRow.Cells[1].Value.ToString();
                 txtnombrescliente.Text = dgvClientes.CurrentRow.Cells[2].Value.ToString();
@@ -180,7 +190,7 @@ namespace GS_Factura
         }
 
         private void btnlimpiardatos_Click(object sender, EventArgs e)
-        {
+        {            
             LimpiarCampos();
         }
 
@@ -188,8 +198,10 @@ namespace GS_Factura
         {
             try
             {
+                // Buscar clientes por cédula mientras se escribe en el campo de texto
                 AccesoDatos.abrirConexion();
                 AccesoDatos rt = new AccesoDatos();
+                // se llena la tabla con el resultando que traiga el metodo retornaClientebuscar
                 dgvClientes.DataSource = rt.retornaClientebuscar(txtcedulacliente.Text);
                 AccesoDatos.CerrarConexion(AccesoDatos.abrirConexion());
             }
@@ -203,6 +215,7 @@ namespace GS_Factura
         {
             try
             {
+                // Buscar clientes por nombre mientras se escribe en el campo de texto
                 AccesoDatos.abrirConexion();
                 AccesoDatos rt = new AccesoDatos();
                 dgvClientes.DataSource = rt.retornaClientebuscar(txtnombrescliente.Text);
@@ -218,6 +231,7 @@ namespace GS_Factura
         {
             try
             {
+                // Buscar clientes por apellido mientras se escribe en el campo de texto
                 AccesoDatos.abrirConexion();
                 AccesoDatos rt = new AccesoDatos();
                 dgvClientes.DataSource = rt.retornaClientebuscar(txtapellidoscliente.Text);
@@ -231,6 +245,7 @@ namespace GS_Factura
 
         private void txtcedulacliente_KeyPress(object sender, KeyPressEventArgs e)
         {
+            // Permitir solo dígitos en el campo de cédula
             if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
             {
                 e.Handled = true;
@@ -239,6 +254,7 @@ namespace GS_Factura
 
         private void txtnombrescliente_KeyPress(object sender, KeyPressEventArgs e)
         {
+            // Permitir solo letras, espacios y retroceso en el campo de nombres
             if (!(char.IsLetter(e.KeyChar) || e.KeyChar == ' ' || e.KeyChar == (char)Keys.Back))
             {
                 e.Handled = true;
@@ -247,6 +263,7 @@ namespace GS_Factura
 
         private void txtapellidoscliente_KeyPress(object sender, KeyPressEventArgs e)
         {
+            // Permitir solo letras, espacios y retroceso en el campo de apellidos
             if (!(char.IsLetter(e.KeyChar) || e.KeyChar == ' ' || e.KeyChar == (char)Keys.Back))
             {
                 e.Handled = true;
