d7a5bb3e8c2f4f3140a9143124d753a86982bb0a
eduardo96452 <107895582+eduardo96452@users.noreply.github.com>
2024-03-13 18:28:07 -0500

    cambios

--- GS-Factura/Clases/AccesoDatos.cs
+++ GS-Factura/Clases/AccesoDatos.cs
@@ -24,12 +24,6 @@ namespace GS_Factura
         {
 
         }
-        public static string ObtenerCadenaConexion()
-        {
-            return cadenaConexion;
-        }
-
-
         public static SqlConnection AbrirConexion()
         {
             try
@@ -43,110 +37,6 @@ namespace GS_Factura
                 MessageBox.Show(ex.Message);
                 return null; // Devuelve null en caso de error.
             }
-        }
-
-        public static void CerrarConexion(SqlConnection conexion)
-        {
-            try
-            {
-                if (conexion.State == System.Data.ConnectionState.Open)
-                {
-                    conexion.Close();
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        public static DataTable LlenarTablaparaBuscar(string consul)
-        {
-            DataTable tabla = new DataTable();
-            try
-            {
-                AbrirConexion();
-                tabla = new DataTable();
-                string consultar = consul;
-                SqlCommand comando = new SqlCommand(consultar, AbrirConexion());
-                SqlDataAdapter data = new SqlDataAdapter(comando);
-                data.Fill(tabla);
-                return tabla;
-            }
-            catch (Exception sms1)
-            {
-                MessageBox.Show(sms1.Message);
-                return tabla;
-            }
-        }
-
-        public static DataTable LlenarTablaparaBuscarComando(string consulta, SqlParameter[] parametros = null)
-        {
-            DataTable tabla = new DataTable();
-
-            try
-            {
-                using (SqlConnection conexion = AbrirConexion())
-                {
-                    using (SqlCommand comando = new SqlCommand(consulta, conexion))
-                    {
-                        if (parametros != null)
-                        {
-                            comando.Parameters.AddRange(parametros);
-                        }
-
-                        SqlDataAdapter data = new SqlDataAdapter(comando);
-                        data.Fill(tabla);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-
-            return tabla;
-        }
-
-
-        public DataTable RetornaClienteBuscar(string buscar)
-        {
-            DataTable dt = new DataTable();
-            try
-            {
-                AbrirConexion();
-                SqlCommand cmd = new SqlCommand("SELECT IDCLIENTE AS ID, RTRIM(CEDULA) AS Cedula, RTRIM(NOMBRE) AS Nombre, RTRIM(APELLIDOS) AS Apellido, FECHA_NACIMIENTO AS 'Fecha Nacimiento', CASE Estado WHEN 0 THEN 'Eliminado' WHEN 1 THEN 'Activo' END AS Estado FROM CLIENTE WHERE (CEDULA like '" + buscar + "%' or NOMBRE LIKE('" + buscar + "%') OR APELLIDOS LIKE '" + buscar + "%') AND Estado = 1;", AbrirConexion());
-                SqlDataAdapter da = new SqlDataAdapter(cmd);
-                da.Fill(dt);
-                return dt;
-            }
-            catch (Exception sms)
-            {
-                MessageBox.Show(sms.Message);
-                return dt;
-            }
-        }
-
-        public static DataTable RetornaRegistros(string Sentencia)
-        {
-            DataTable dt = new DataTable();
-            try
-            {
-                SqlConnection oCon = AbrirConexion(); // Utiliza el método AbrirConexion para obtener la conexión.
-                SqlCommand ocom = new SqlCommand(Sentencia, oCon);
-                SqlDataAdapter oDA = new SqlDataAdapter(ocom);
-                oDA.Fill(dt);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-                dt = null;
-            }
-
-            return dt;
-        }
-
-        
-    }
-        
+        }        
+    }        
 }
